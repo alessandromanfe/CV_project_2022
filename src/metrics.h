403659e622742accc67bfa0b75964cd52cc453a6
alessandromanfe <alessandro.manfe98@gmail.com>
2022-07-23 22:01:33 +0200

    Added metrics

--- src/metrics.h
+++ src/metrics.h
@@ -2,8 +2,9 @@
 #define metrics
 
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
-namespace metrics {
+namespace metric {
 	/**
 	 * Computes pixel accuracy for both hand and non-hand segmentation
 	 * @param x generated mask to be evaluated
@@ -12,6 +13,14 @@ namespace metrics {
 	 * @return 1 if the two matrices are same size and one channel, -1 otherwise
 	 */
 	int pixelAccuracy(const cv::Mat& x, const cv::Mat& y, std::vector<double>& v);
+
+	/**
+	 * Computes Intersection over Union for the given set of boxes
+	 * @param boxes generated boxes to be evaluated
+	 * @param real ground truth boxes
+	 * @return IoU metric [0,1] as a double
+	 */
+	double IoU(const std::vector<cv::Rect>& boxes, const std::vector<cv::Rect>& real);
 }
 
 #endif
